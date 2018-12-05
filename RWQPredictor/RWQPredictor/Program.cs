7a5e47c1a999d3d15749ebf646d3202e857fcafb
Marco Milani <emme.milani@hotmail.it>
2018-12-05 23:07:23 +0100

    Enable C# 7.1

--- RWQPredictor/RWQPredictor/Program.cs
+++ RWQPredictor/RWQPredictor/Program.cs
@@ -1,10 +1,15 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace RWQPredictor
 {
     class Program
     {
-        static void Main(string[] args)
+        private static string TrainDataPath = "winequality-red.csv";
+        private static string TestDataPath = "winequality_test.csv";
+        private static string ModelPath = "MLModels/WineClassificationModel.zip";
+        
+        static async Task Main(string[] args)
         {
             Console.WriteLine("Hello World!");
         }
